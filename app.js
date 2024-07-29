7301a6290e497266ff68223ccc977202a604c447
adayilmax <144628453+adayilmax@users.noreply.github.com>
2024-07-29 19:05:38 +0300

    03

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js"
 
-const app = express()
-const port = 3000
+dotenv.config();
+
+//connection to the db
+conn();
+
+const app = express();
+const port = process.env.PORT;
 
 //ejs template engine
 app.set("view engine", "ejs");
